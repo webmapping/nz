cfaf8337dd35988f49e5a3ce320619a72b9dbae5
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-03-20 18:12:20 +0100

    Karte nach main.js ausgelagert

---
+++ main.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([-39.2, 175.583333], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([-39.2, 175.583333]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
