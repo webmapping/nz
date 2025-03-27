28fb523c1ea57d7cb670797ef8168f2e16cb0092
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-03-27 18:43:30 +0100

    auf Änderungen beim Pulldown reagieren

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const STOPS = [
     {
         nr: 4,
         title: "Cormandel Peninsula",
-        usr: "Gregorysprenger2001",
+        user: "Gregorysprenger2001",
         lat: -37.882778,
         lng: 175.636667,
         zoom: 11,
@@ -228,4 +228,21 @@ for (let i = 0; i < STOPS.length; i++) {
         map.setView([STOPS[i].lat, STOPS[i].lng], STOPS[i].zoom);
         marker.openPopup();
     }
+
+    // Pulldownmenü befüllen
+    let option = document.createElement("option");
+    option.value = STOPS[i].user;
+    option.text = STOPS[i].title;
+    if (STOPS[i].user == "webmapping") {
+        option.selected = true;
+    }
+    document.querySelector("#pulldown select").appendChild(option);
+}
+
+// auf Änderungen beim Pulldown reagieren
+document.querySelector("#pulldown select").onchange = function(evt) {
+    let url = `https://${evt.target.value}.github.io/nz`;
+    //console.log(evt.target.value);
+    //console.log(url);
+    window.location = url;
 }
