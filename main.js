0826f781ace98a381b0e091807ce8c2388304eae
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-03-27 15:38:34 +0100

    var durch let ersetzt

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ Skript für die Neuseelandreise
 */
 
 // Karte initialisieren
-var map = L.map('map').setView([-39.2, 175.583333], 11);
+let map = L.map('map').setView([-39.2, 175.583333], 11);
 
 // Hintergrundkarte definieren
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,7 +12,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Marker zeichnen
-var marker = L.marker([-39.2, 175.583333]).addTo(map);
+let marker = L.marker([-39.2, 175.583333]).addTo(map);
 
 // Popup definieren und öffnen
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
