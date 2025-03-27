68916b85b34ffdca68b2b636d237afa0bfa4342c
Klaus Förster <klaus.foerster@uibk.ac.at>
2025-03-27 17:20:30 +0100

    Fehler im Etappenobjekt bereinigt

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const STOPS = [
         lng: 172.6775,
         zoom: 13,
         title: "Cape Reinga",
-        nummer: 1,
+        nr: 1,
         user: "vintiyannick",
     },
     {
@@ -25,7 +25,7 @@ const STOPS = [
         title: "Bay of Islands",
         user: "Ellinnaa",
         lat: -35.183333,
-        long: 174.166667,
+        lng: 174.166667,
         zoom: 11,
     },
     {
@@ -120,9 +120,9 @@ const STOPS = [
         nr: 16,
         title: "Wanaka",
         user: "lizzie2911",
-        lat: lat,
-        lon: lon,
-        zoom: zoom,
+        lat: -44.7,
+        lon: 169.15,
+        zoom: 13,
     },
     {
         nr: 17,
